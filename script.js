d47a78ad280afdc825cd64e1b5b7ac71f84c2dcc
Mathias <71843132+mathiasvck@users.noreply.github.com>
2021-02-02 16:51:59 +0100

    overhaul
    add images and links for events
    adapt media screens (ipad mobile)
    filter buttons added (js doesn't work)

--- script.js
+++ script.js
@@ -15,3 +15,37 @@ function searchBar() {
       }
   }
 }
+/* Filter Buttons */
+filterSelection("all")
+function filterSelection(filterName) {
+    var x, i;
+    x = document.getElementsByClassName("filterDiv");
+    if (filterName == "all") filterName = "";
+    for (i = 0; i < x.length; i++) {
+        removeClass(x[i], "show");
+        if (x[i].className.indexOf(filterName) > -1) addClass(x[i], "show");
+    }
+}
+
+function addClass (element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i=0; i < arr2.length; i++) {
+        if (arr1.indexOf(arr2[i]) == -1) {
+            element.className += " " + arr2[i];
+        }
+    }
+}
+
+function removeClass (element, name) {
+    var i, arr1, arr2;
+    arr1 = element.className.split(" ");
+    arr2 = name.split(" ");
+    for (i=0; i < arr2.length; i++) {
+        while (arr1.indexOf(arr2[i]) > -1) {
+            arr1.splice(arr1.indexOf(arr2[i]), 1)
+        }
+    }
+    element.className = arr1.join(" ");
+}
